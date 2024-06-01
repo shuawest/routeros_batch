dfd236741f5020322f437d67bdf1a75d838a5c05
Josh West <shuawest@gmail.com>
2024-06-01 17:04:28 -0400

    updated docs

--- ansible_collections/shuawest/routeros_batch/plugins/modules/script.py
+++ ansible_collections/shuawest/routeros_batch/plugins/modules/script.py
@@ -29,93 +29,94 @@ options:
       - Comment to add to the script
     required: false
     type: str
-  verbose_steps:
-    description:
-      - Show the result of each individual step in the output
-    required: false
-    type: bool
-  state:
-    description:
-      - State of the script for the task execution
-    required: true
-    type: str
-    choices: [ "present", "absent", "executed", "executed_once", "executed_clean" ]
-  content:
-    description:
-      - Direct content to add to the script
-      - Mutually exclusive with `commands`
-    required: false
-    type: str
-  commands:
-    description:
-      - List of commands to generate script
-      - Mutually exclusive with `content`
-    required: false
-    type: list
-    elements: dict
-    suboptions:
-      desc:
-        description:
-          - Description of the command
-        required: true
-        type: str
-      path:
-        description:
-          - Full path to the configuration object
-        required: true
-        type: str
-      state:
-        description:
-          - State of the configuration object
-        required: true
-        type: str
-        choices: [ "present", "absent" ]
-      values:
-        description:
-          - List of key-value pairs to set on the configuration object
-        required: false
-        type: list
-        elements: dict
-        suboptions:
-          attr:
-            description:
-              - Attribute name
-            required: true
-            type: str
-          value:
-            description:
-              - Value to set
-            required: true
-            type: str
-          mode:
-            description:
-              - Mode to use for this attribute value in the script
-              - Refer to the `routeros_batch.scriptgen` module for more information
-  routeros:
-    description:
-      - Connection details to the Mikrotik RouterOS device
-      - Refer to the community.routeros.api module parameters for more information
-    required: true
-    type: dict
-    suboptions:
-      hostname:
-        description:
-          - Hostname or IP address of the RouterOS device
-        required: true
-        type: str
-      username:
-        description:
-          - Username to authenticate with
-        required: true
-        type: str
-      password:
-        description:
-          - Password to authenticate with
-        required: true
-        type: str
-seealso:
-  - module: community.routeros.api
-'''
+  '''
+  # verbose_steps:
+  #   description:
+  #     - Show the result of each individual step in the output
+  #   required: false
+  #   type: bool
+  # state:
+  #   description:
+  #     - State of the script for the task execution
+  #   required: true
+  #   type: str
+  #   choices: [ "present", "absent", "executed", "executed_once", "executed_clean" ]
+  # content:
+  #   description:
+  #     - Direct content to add to the script
+  #     - Mutually exclusive with `commands`
+  #   required: false
+  #   type: str
+  # commands:
+  #   description:
+  #     - List of commands to generate script
+  #     - Mutually exclusive with `content`
+  #   required: false
+  #   type: list
+  #   elements: dict
+  #   suboptions:
+  #     desc:
+  #       description:
+  #         - Description of the command
+  #       required: true
+  #       type: str
+#       path:
+#         description:
+#           - Full path to the configuration object
+#         required: true
+#         type: str
+#       state:
+#         description:
+#           - State of the configuration object
+#         required: true
+#         type: str
+#         choices: [ "present", "absent" ]
+#       values:
+#         description:
+#           - List of key-value pairs to set on the configuration object
+#         required: false
+#         type: list
+#         elements: dict
+#         suboptions:
+#           attr:
+#             description:
+#               - Attribute name
+#             required: true
+#             type: str
+#           value:
+#             description:
+#               - Value to set
+#             required: true
+#             type: str
+#           mode:
+#             description:
+#               - Mode to use for this attribute value in the script
+#               - Refer to the `routeros_batch.scriptgen` module for more information
+#   routeros:
+#     description:
+#       - Connection details to the Mikrotik RouterOS device
+#       - Refer to the community.routeros.api module parameters for more information
+#     required: true
+#     type: dict
+#     suboptions:
+#       hostname:
+#         description:
+#           - Hostname or IP address of the RouterOS device
+#         required: true
+#         type: str
+#       username:
+#         description:
+#           - Username to authenticate with
+#         required: true
+#         type: str
+#       password:
+#         description:
+#           - Password to authenticate with
+#         required: true
+#         type: str
+# seealso:
+#   - module: community.routeros.api
+# '''
 
 EXAMPLES = '''
 - name: Run a set of commands converted to a routeros script on a Mikrotik device
@@ -194,8 +195,6 @@ script:
   description: RouterOS script content
   type: string
   returned: always
-  sample:
-  - "; ### present: Put VPN bridge 'bridge' ### ;\n\n:if ([:len [/interface bridge find name=\\\"bridge\\\" ]] > 0) do={\n\t/interface bridge set [ find name=\\\"bridge\\\" ] name=\\\"bridge\\\" fast-forward=no \n} else={\n\t/interface bridge add name=\\\"bridge\\\" fast-forward=no \n}\n\n\n; ### absent: Remove VPN user 'guest' ### ;\n\n:if ([:len [/ppp secret find name=\\\"guest\\\" ]] > 0) do={\n\t/ppp secret remove [ find name=\\\"guest\\\" ]\n}\n\n\n",
 state:
   description: State of the script for the task execution
   type: string
